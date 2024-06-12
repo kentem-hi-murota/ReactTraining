3968159762a9ba82b86157a7e9c24f8a3dffb8ed
hi-murota <hi-murota@kentem.co.jp>
2024-06-12 15:28:45 +0900

    カードの情報表示の調整

--- src/Components/PokemonApp/Components/Card/Card.tsx
+++ src/Components/PokemonApp/Components/Card/Card.tsx
@@ -10,14 +10,18 @@ const Card = ({ pokemon }: CardProps) => {
     <div key={pokemon.id} css={cardStyle}>
       <img src={pokemon.sprites.front_default} alt={pokemon.name}></img>
       <h2 css={h2Style}>{pokemon.name}</h2>
-      <p>
-        タイプ <br />
-        {pokemon.types[0].type.name} <br />
-        {pokemon.types[1] && pokemon.types[1].type.name}
-      </p>
-      <p>重さ：{pokemon.weight}</p>
-      <p>高さ：{pokemon.height}</p>
-      <p>アビリティ：{pokemon.abilities[0].ability.name}</p>
+      <div css={typeStyle}>
+        <span css={css({ textAlign: "right", alignContent: "center" })}>
+          タイプ：
+        </span>
+        <span css={css({ textAlign: "left" })}>
+          {pokemon.types[0].type.name} <br />
+          {pokemon.types[1] && pokemon.types[1].type.name}
+        </span>
+      </div>
+      <p>たかさ：{(pokemon.height / 10).toFixed(1)} m</p>
+      <p>おもさ：{(pokemon.weight / 10).toFixed(1)} kg</p>
+      <p>とくせい：{pokemon.abilities[0].ability.name}</p>
     </div>
   );
 };
@@ -32,6 +36,12 @@ const cardStyle = css({
 
 const h2Style = css({
   margin: 0,
+});
+
+const typeStyle = css({
+  display: "grid",
+  gridTemplateColumns: "1.2fr 1fr",
+  gap:'0.2em'
 });
 
 export default Card;
