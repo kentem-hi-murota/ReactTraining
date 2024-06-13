9c5d054ad3868e0ea5c557c98f360c5e2a83d647
hi-murota <hi-murota@kentem.co.jp>
2024-06-13 16:15:09 +0900

    各ページのコンポーネントファイル作成

--- src/Components/Blog/Blog.tsx
+++ src/Components/Blog/Blog.tsx
@@ -1,6 +1,17 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home, CreatePost, Login, Logout } from "./Components";
+
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/createpost" element={<CreatePost />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default Blog;
