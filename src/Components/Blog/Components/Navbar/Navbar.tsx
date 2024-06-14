a2e266251aceb462cfbd6cfa79121fbde862bbb7
hi-murota <hi-murota@kentem.co.jp>
2024-06-14 09:36:26 +0900

    navbarスタイリングのためにfontawsomeを導入、スタイリング

--- src/Components/Blog/Components/Navbar/Navbar.tsx
+++ src/Components/Blog/Components/Navbar/Navbar.tsx
@@ -1,14 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { css } from "@emotion/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faFilePen,
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to="/">ホーム</Link>
-      <Link to="/createpost">記事投稿</Link>
-      <Link to="/login">ログイン</Link>
+    <nav css={navVarStyle}>
+      <Link to="/" css={linkStyle}>
+        <FontAwesomeIcon icon={faHouse} />
+        ホーム
+      </Link>
+      <Link to="/createpost" css={linkStyle}>
+        <FontAwesomeIcon icon={faFilePen} />
+        記事投稿
+      </Link>
+      <Link to="/login" css={linkStyle}>
+        <FontAwesomeIcon icon={faArrowRightToBracket} />
+        ログイン
+      </Link>
     </nav>
   );
 };
+
+const navVarStyle = css({
+  display: "flex",
+  justifyContent: "center",
+  gap: "32px",
+  background: "#EEE",
+  width: "100%",
+  padding: "16px 0",
+});
+
+const linkStyle = css({
+  all: "unset",
+  borderRadius: "4px",
+  background: "#444",
+  color: "white",
+  padding: "4px 16px",
+
+  "&:hover": css({
+    cursor: "pointer",
+    background: "#888",
+  }),
+
+  svg: css({
+    marginRight: "5px",
+  }),
+});
 
 export default Navbar;
