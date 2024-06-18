80423d653431f094ef36f6f52cba634acba03f30
hi-murota <hi-murota@kentem.co.jp>
2024-06-18 09:59:20 +0900

    プレビューコンポーネントに要素を追加してスタイリング

--- src/Components/Note/Components/Preview/Preview.tsx
+++ src/Components/Note/Components/Preview/Preview.tsx
@@ -1,5 +1,26 @@
+import { css } from '@emotion/react';
+
 const Preview = () => {
-  return <div>Preview</div>;
+  return (
+    <div css={previewStyle}>
+      <h2>タイトル</h2>
+      <p>ノート内容</p>
+    </div>
+  );
 };
+
+const previewStyle = css({
+  textAlign: 'left',
+  height: 'calc(50vh - 16px)',
+  margin: '16px 16px',
+  borderTop: '1px solid #bbb',
+  overflowY: 'scroll',
+  background: 'rgba(0,0,0,0.1)',
+
+  'h2,p': {
+    margin: 0,
+    padding: '12px',
+  },
+});
 
 export default Preview;
