315b9c373c8b4dd28104300cfd46fc7d82ed1909
hi-murota <hi-murota@kentem.co.jp>
2024-06-19 11:21:33 +0900

    ビンゴシートのスタイリング

--- src/Components/Bingo/Bingo.tsx
+++ src/Components/Bingo/Bingo.tsx
@@ -1,3 +1,5 @@
+import { css } from '@emotion/react';
+
 function Bingo() {
   const getCandidateNumbersArray = (rowIndex: number): number[] => {
     return [...Array(15)].map((_, i) => i + 1 + 15 * (rowIndex - 1));
@@ -20,15 +22,21 @@ function Bingo() {
   const bingoSheet: string[][] = generateBingoSheet();
 
   return (
-    <div>
-      {'BINGO'.split('').map((char) => (
-        <div key={char}>{char}</div>
-      ))}
+    <div css={sheetStyle}>
+      <div css={rowStyle} className="rowHeader">
+        {'BINGO'.split('').map((char) => (
+          <div key={char} css={cellStyle}>
+            {char}
+          </div>
+        ))}
+      </div>
       {bingoSheet.map((row, index) => {
         return (
-          <div key={index}>
+          <div key={index} css={rowStyle} className="rowBody">
             {row.map((number) => (
-              <div key={number}>{number}</div>
+              <div key={number} css={cellStyle}>
+                {number}
+              </div>
             ))}
           </div>
         );
@@ -36,5 +44,31 @@ function Bingo() {
     </div>
   );
 }
+
+const sheetStyle = css({
+  background: 'white',
+  width: 'calc(42px * 5 + 2px * 4)',
+  margin: '24px auto 0',
+});
+
+const rowStyle = css({
+  display: 'flex',
+  gap: '2px',
+
+  '&.rowHeader': {
+    fontWeight: 'bold',
+  },
+
+  '&.rowBody': css({
+    marginTop: '2px',
+  }),
+});
+
+const cellStyle = css({
+  background: 'lightpink',
+  width: '42px',
+  height: '42px',
+  alignContent: 'center',
+});
 
 export default Bingo;
