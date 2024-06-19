7d45bdabc0465c64814e7efc73c0ad3d17def5cf
hi-murota <hi-murota@kentem.co.jp>
2024-06-19 10:59:34 +0900

    ビンゴシートの番号を生成する処理を実装

--- src/Components/Bingo/Bingo.tsx
+++ src/Components/Bingo/Bingo.tsx
@@ -1,5 +1,31 @@
 function Bingo() {
-  return <div>Bingo</div>;
+  const getCandidateNumbersArray = (rowIndex: number): number[] => {
+    return [...Array(15)].map((_, i) => i + 1 + 15 * (rowIndex - 1));
+  };
+
+  const shuffleArray = (array: number[]): number[] => array.sort(() => 0.5 - Math.random());
+  const transposeArray = (array: string[][]): string[][] => array[0].map((_, c) => array.map((r) => r[c]));
+
+  const generateBingoSheet = (): string[][] => {
+    const bingoSheet: string[][] = [...Array(5)].map(() => [...Array(0)]);
+    for (let row = 1; row < 6; row++) {
+      const candidate: number[] = shuffleArray(getCandidateNumbersArray(row));
+      bingoSheet[row - 1] = candidate.slice(0, 5).toString().split(',');
+    }
+    bingoSheet[2][2] = 'FREE';
+
+    return transposeArray(bingoSheet);
+  };
+
+  const bingoSheet: string[][] = generateBingoSheet();
+
+  return (
+    <div>
+      {'BINGO'.split('').map((char) => (
+        <div key={char}>{char}</div>
+      ))}
+    </div>
+  );
 }
 
 export default Bingo;
