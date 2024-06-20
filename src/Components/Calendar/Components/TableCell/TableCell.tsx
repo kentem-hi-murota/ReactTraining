887601114bdcf50644c8bdc00136a7b661515c01
hi-murota <hi-murota@kentem.co.jp>
2024-06-20 13:50:26 +0900

    TableCellコンポーネントの実装

--- src/Components/Calendar/Components/TableCell/TableCell.tsx
+++ src/Components/Calendar/Components/TableCell/TableCell.tsx
@@ -1,5 +1,27 @@
-function TableCell() {
-  return <td></td>;
+import { SerializedStyles } from '@emotion/react';
+interface Props {
+  style?: SerializedStyles | SerializedStyles[];
+  value: number | string;
+  className?: string;
+  colSpan?: number;
+  isHeader?: boolean;
+  clickHandler?: () => void;
 }
+
+const TableCell = ({ style, value, className, colSpan, isHeader = false, clickHandler }: Props) => {
+  return (
+    <>
+      {isHeader ? (
+        <th css={style} colSpan={colSpan} className={className} onClick={clickHandler}>
+          {value}
+        </th>
+      ) : (
+        <td css={style} colSpan={colSpan} className={className}>
+          {value}
+        </td>
+      )}
+    </>
+  );
+};
 
 export default TableCell;
