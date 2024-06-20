b4aa8cbe9e2f824258abe98ec4880ec7748ddf4f
hi-murota <hi-murota@kentem.co.jp>
2024-06-20 15:57:46 +0900

    スケジュール表示のためのコンポーネントファイル追加

---
+++ src/Components/Calendar/Components/UList/UList.tsx
@@ -0,0 +1,11 @@
+const UList = () => {
+  return (
+    <ul>
+      <li>a</li>
+      <li>a</li>
+      <li>a</li>
+    </ul>
+  );
+};
+
+export default UList;
---
+++ src/Components/Calendar/Components/UList/index.ts
@@ -0,0 +1,1 @@
+export { default as UList } from "./UList.tsx";
