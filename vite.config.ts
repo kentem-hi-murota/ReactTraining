80e919d594b0209a54bdc2ceb0f1fce45908a603
hi-murota <hi-murota@kentem.co.jp>
2024-06-26 09:01:11 +0900

    テストのライブラリ追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxImportSource: "@emotion/react" })],
+  plugins: [react({ jsxImportSource: '@emotion/react' })],
+  test: {
+    globals: true,
+    environment: 'jsdom', // テスト環境をjsdomに設定
+    setupFiles: './vitest.setup.ts', // セットアップファイル
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
 });
+
